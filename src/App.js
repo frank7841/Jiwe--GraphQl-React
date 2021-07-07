e1a4630126af02f8b7f304286e4521841830996d
frank7841 <franklinewax@gmail.com>
2021-07-07 15:18:32 +0300

    setting up working environment

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/main.css';
+
+
+function App () {
+  return (
+   <div className="centered-text">
+     <h1>Jiwe Test</h1>
+     <p>Consuming Graphql API</p>
+   </div>
+  );
+}
+
+export default App;
